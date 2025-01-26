d13446424c6d4d329017b99003587edee8d6f941
harshanet <harshajoshiofficial@gmail.com>
2025-01-26 14:14:53 +1000

    Update test.py

--- all/deco3801/space-buns/entities/test.py
+++ all/deco3801/space-buns/entities/test.py
@@ -1,1 +1,11 @@
+import os
 
+def check_order(directory):
+    files = os.listdir(directory)
+    sorted_files = sorted(files)
+    if files != sorted_files:
+        print("Files are not in alphabetical order.")
+    else:
+        print("Files are sorted alphabetically.")
+
+check_order(".")
